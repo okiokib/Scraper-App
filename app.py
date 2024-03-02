8989e64b8af51de6ff1c7fb45f05f4a3a10e5707
okibagus <111863907+okiokib@users.noreply.github.com>
2024-03-02 12:22:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
-from datetime import datetime, timedelta
-import streamlit as st
-import base64
+from datetime import date, timedelta
+from requests_html import HTMLSession
 
 def get_article_content(article_url):
-    response = requests.get(article_url)
+    session = HTMLSession()
+    response = session.get(article_url)
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        article_content = soup.find('div', class_='read__content').find_all('p')
-        content = ''.join([p.get_text() for p in article_content])
-        return content
+        content = response.html.find('div.read__content p')
+        return ''.join([p.text for p in content])
     return ""
 
-def scrape_news_data(theme, start_date, end_date):
-    base_url_dict = {
-        "Health": "https://health.kompas.com/search/{}-{}-{}",
-        "Travel": "https://travel.kompas.com/search/{}-{}-{}",
-        "Sports": "https://www.kompas.com/sports/search/{}-{}-{}",
-        "Education": "https://edukasi.kompas.com/search/{}-{}-{}",
-        "Technology": "https://tekno.kompas.com/search/{}-{}-{}",
-        "Property": "https://properti.kompas.com/search/{}-{}-{}",
-        "Food": "https://www.kompas.com/food/search/{}-{}-{}"
-    }
-
-    base_url = base_url_dict.get(theme, "")
-    if not base_url:
-        st.error("Invalid theme selection.")
-        return None
-
+def scrape_news_data(start_date, end_date):
+    base_url = "https://health.kompas.com/search/{}-{}-{}"
     data = []
 
     current_date = start_date
     while current_date <= end_date:
         url = base_url.format(current_date.year, current_date.month, current_date.day)
-        response = requests.get(url)
+        session = HTMLSession()
+        response = session.get(url)
 
         if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            news_elements = soup.find_all('h3', class_='article__title article__title--medium')
+            news_elements = response.html.find('h3.article__title.article__title--medium')
 
             for element in news_elements:
-                title = element.a.text
-                article_url = element.a['href']
+                title = element.text
+                article_url = element.find('a', first=True).attrs['href']
 
-                article_response = requests.get(article_url)
-                article_soup = BeautifulSoup(article_response.text, 'html.parser')
-
-                date_time_element = article_soup.find('div', class_='read__time')
-                date_time = date_time_element.text.strip() if date_time_element else ""
-                date_time = date_time.replace('Kompas.com -', '')
-
-                article_link_element = article_soup.find('a', class_='article__link')
-                article_link = article_link_element['href'] if article_link_element else ""
-
-                class_element = article_soup.find('div', class_='article__subtitle article__subtitle--inline')
-                class_info = class_element.text.strip() if class_element else ""
+                article_response = session.get(article_url)
+                date_time = article_response.html.find('div.read__time', first=True).text.replace('Kompas.com -', '').strip()
+                article_link = article_response.html.find('a.article__link', first=True).attrs['href']
+                class_info = article_response.html.find('div.article__subtitle.article__subtitle--inline', first=True).text.strip()
 
                 content = get_article_content(article_url)
 
@@ -70,34 +44,13 @@ def scrape_news_data(theme, start_date, end_date):
 
             current_date += timedelta(days=1)
         else:
-            st.error(f"Failed to fetch data for {current_date.strftime('%d-%m-%Y')}")
-            return None
+            print(f"Failed to fetch data for {current_date.strftime('%d-%m-%Y')}")
 
-    return pd.DataFrame(data)
+    return data
 
-# Streamlit app
-st.title("News Scraper App")
+start_date = date(2024, 2, 23)
+end_date = date(2024, 2, 24)
 
-theme_options = ["Health", "Travel", "Sports", "Education", "Technology", "Property", "Food"]
-selected_theme = st.selectbox("Select Theme:", theme_options)
-
-start_date = st.date_input("Start Date", datetime.now() - timedelta(days=7))
-end_date = st.date_input("End Date", datetime.now())
-
-if start_date > end_date:
-    st.error("End date must be greater than or equal to start date.")
-else:
-    if st.button("Scrape News"):
-        st.info("Scraping news data...")
-        df = scrape_news_data(selected_theme, start_date, end_date)
-
-        if df is not None and not df.empty:
-            st.write(df)
-
-            # Download link for CSV file
-            csv = df.to_csv(index=False)
-            b64 = base64.b64encode(csv.encode()).decode()
-            href = f'<a href="data:file/csv;base64,{b64}" download="news_data.csv">Download CSV File</a>'
-            st.markdown(href, unsafe_allow_html=True)
-        else:
-            st.warning("No data available.")
+news_data_health = scrape_news_data(start_date, end_date)
+df_health = pd.DataFrame(news_data_health)
+df_health
